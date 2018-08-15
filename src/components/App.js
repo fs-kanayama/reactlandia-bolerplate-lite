085c1d66b0ff3bd9be42e8a44c3a183aa2fae3f2
ivan.aksamentov <ivan.aksamentov@gmail.com>
2018-08-16 01:12:05 +0200

    Format

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {setConfig, hot} from 'react-hot-loader'
+import { setConfig, hot } from 'react-hot-loader'
 
 import Switcher from './Switcher'
 import Nav from './Nav'
 
 const App = () => (
-    <div>
-        <Nav/>
-        <Switcher/>
-    </div>
+  <div>
+    <Nav/>
+    <Switcher/>
+  </div>
 )
 
 setConfig({ logLevel: 'warn' })
