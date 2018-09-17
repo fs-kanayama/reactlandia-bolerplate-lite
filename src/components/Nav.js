0929cc86dbaad2bba26510cc9126e6ed50d1c264
ivan.aksamentov <ivan.aksamentov@gmail.com>
2018-09-17 16:45:53 +0200

    Add an example of nested routes with parameters

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,6 +13,9 @@ const links = {
   '/thing/1': 'Thing 1',
   '/thing/2': 'Thing 2',
   '/thing/amazing-thingy': 'Thing "Amazing Thingy"',
+  '/tree/upper/red': 'Nest: upper, Bird: red',
+  '/tree/left/blue': 'Nest: left, Bird: blue',
+  '/tree/right': 'Nest: right, Bird: none',
   '/does-not-exist': 'Does not exist',
 }
 
